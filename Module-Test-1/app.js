5009728d0e2c0cae8cd5aae75479cbccb75a5ac5
kumawat30rahul <kumawat30rahul@gmail.com>
2022-12-30 19:31:48 +0530

    intial ommit

---
+++ Module-Test-1/app.js
@@ -0,0 +1,6 @@
+function alert(){
+    const success = document.getElementById('form-alert')
+    const submitting = document.getElementById('form-submit')
+
+    submitting.onclick.style.display = 'block'
+}
